ba5b9da7cae27f361d71eb8ee997aab8127b74cd
Modupeoluwa Oluwatayo <othanksgiving@gmail.com>
2024-06-27 16:09:02 +0100

    convert column to string update

--- hackathon.py
+++ hackathon.py
@@ -1,17 +1,22 @@
 
 import csv
 
-def convertListToString(file_path):
-    
-    with open(file_path) as csv_file:
+def convertCsvToList(file_path):
+
+    with open('C://Users//user//Downloads//Hackathon2024.csv') as csv_file:
         csv_reader = csv.DictReader(csv_file)
    
         dict_from_csv = dict(list(csv_reader)[0])
  
 
         list_of_column_names = list(dict_from_csv.keys())
-        column_names_string = ''.join(list_of_column_names)
-        print(column_names_string)
+        print(list_of_column_names)
+        return list_of_column_names
+        
+
+
+    
+    
 
  
         
