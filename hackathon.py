bdc75f1396ae6a26bd6e45adb8ed62fbce094f0c
Modupeoluwa Oluwatayo <othanksgiving@gmail.com>
2024-06-27 15:53:35 +0100

    convert column to string update

--- hackathon.py
+++ hackathon.py
@@ -1,8 +1,17 @@
 
-import pandas as pd
+import csv
+
+with open('C://Users//user//Downloads//Hackathon2024.csv') as csv_file:
  
-df = pd.read_csv('data.csv')
+        # reading the csv file using DictReader
+    csv_reader = csv.DictReader(csv_file)
  
-list_of_column_names = list(df.columns)
-column_names_string = ''.join(list_of_column_names)
-print(column_names_string)
+    # converting the file to dictionary
+    # by first converting to list
+    # and then converting the list to dict
+    dict_from_csv = dict(list(csv_reader)[0])
+ 
+    # making a list from the keys of the dict
+    list_of_column_names = list(dict_from_csv.keys())
+    column_names_string = ''.join(list_of_column_names)
+    print(column_names_string)
